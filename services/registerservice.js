747dfadf4bbb531cc9e4b0d03d3ea98e4557ef3a
PriraRoja <72965298+PriraRoja@users.noreply.github.com>
2020-11-30 21:44:42 +0530

    Update registerservice.js

--- services/registerservice.js
+++ services/registerservice.js
@@ -1,10 +1,16 @@
-class RegisterService{
-    register = function(detail){
-       var usersTemp = JSON.parse(localStorage.getItem("USERS"));
-       var users= usersTemp ? usersTemp:[];
-       users.push(detail);
-       localStorage.setItem("USERS",JSON.stringify(users));
-       let result="success";
-       return result;
+class RegisterService {
+    register = function(detail) {
+        var usersr = JSON.parse(localStorage.getItem("USERS")) || [];
+        if (!usersr.some(user => user.email === detail.email)) {
+            usersr.push(detail);
+            localStorage.setItem("USERS", JSON.stringify(usersr));
+            location.reload();
+            console.log(detail);
+            alert("continue login");
+            let result = "success";
+            return result;
+        } else {
+            alert("This mail-id is alredy taken try new mail-id");
         }
+    }
 }
